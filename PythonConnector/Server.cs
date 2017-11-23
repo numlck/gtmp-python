2a803392ecb0287524a86e419ab2748f14d5d179
mduiker <maikelduiker@live.nl>
2017-11-23 03:56:41 -0800

    First commit

---
+++ PythonConnector/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Shared;
+
+using IronPython.Hosting;
+using IronPython.Runtime;
+using IronPython.Runtime.Types;
+using IronPython;
+
+namespace PythonConnector
+{
+    public class Server
+    {
+        //public static PythonType API = DynamicHelpers.GetPythonTypeFromType(typeof(API));
+        public static API API = new API();
+        public static PythonType Vector3 = DynamicHelpers.GetPythonTypeFromType(typeof(GrandTheftMultiplayer.Shared.Math.Vector3));
+        public static PythonType Quaternion = DynamicHelpers.GetPythonTypeFromType(typeof(GrandTheftMultiplayer.Shared.Math.Quaternion));
+        public static PythonType Matrix = DynamicHelpers.GetPythonTypeFromType(typeof(GrandTheftMultiplayer.Shared.Math.Matrix4));
+
+        public static PythonType WeaponHash = DynamicHelpers.GetPythonTypeFromType(typeof(WeaponHash));
+        public static PythonType VehicleHash = DynamicHelpers.GetPythonTypeFromType(typeof(VehicleHash));
+    }
+}
---
+++ PythonConnector/bin/Debug/PythonConnector0.2/README.txt
@@ -0,0 +1,3 @@
+﻿INSTALL:
+	1. Place resource in "resources" folder of server
+	2. Run server. add python script to meta.xml "pythonstart" 
